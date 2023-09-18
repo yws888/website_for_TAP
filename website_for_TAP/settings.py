f590fb10e204ede9e2d7a95631618611393f7f14
Ubuntu <ubuntu@ip-172-31-41-218.eu-north-1.compute.internal>
2023-09-18 16:21:17 +0000

    deployment changes

--- website_for_TAP/settings.py
+++ website_for_TAP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-p5xuh72ov4lcbx7+k=k!l!d)3hb1+_m$v5&)203^3v^kr5(i@2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['16.171.15.110']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
 
